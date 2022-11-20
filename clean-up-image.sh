636d39f4490adbc52f619f338c80c911b3305bc1
wisnu setiawan <wissnusetiawan@gmail.com>
2022-11-21 01:56:13 +0700

    Update clean-up-image.sh

--- clean-up-image.sh
+++ clean-up-image.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 
+######################################################################
+# Script Name    : clean-up-image.sh
+# Description    : Used to clean up container registries by deleting untagged (dangling) images and images older than 30 days
+# Args           : CONTAINER_REGISTRY_NAME
+# Author         : Wisnu Setiawan <wissnusetiawan@gmail.com>
+######################################################################
+
+# Stop execution on any error
 registry_name=$1
 repository_name=$2
